fab0125d45a63420f4f6d743c0aac4ca5632d308
GAURAV KUMAR <gaurav7654663039@gmail.com>
2021-11-18 01:15:13 +0530

    creates sort function for insertion sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
 
 using namespace std;
+void sort(int *arr, int len);
 
 int main()
 {
-     int arr[10] = {1,2,6,4,78,40,67,100,56,30};
+    int arr[10] = {100,26,7,400,398,352,874,20,48,20};
+    int len = sizeof(arr) / sizeof(int);
 
-     //insertion sort
-     for(int j = 1;j < 10;++j)
-     {
+    sort(arr, len);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << " ";
+    }
+     
+}
+
+void sort(int arr[], int len) {
+    
+    for(int j = 1;j < len;++j) {
         int key = arr[j];
         int i = j - 1;
 
-        //shift the greater number to the right
-        while (i > -1 && arr[i] > key)
-        {
+        while(i > -1 && arr[i] > key) {
             arr[i + 1] = arr[i];
             i--;
         }
-        
-        arr[i + 1] = key;
-     }
 
-     for (int i = 0; i < 10; i++)
-     {
-         cout << arr[i] << " ";
-     }
-     
+        arr[i + 1] = key;
+    }
 }
